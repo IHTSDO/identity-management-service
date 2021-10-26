0c50e3dd6f66f5044604aa0377ad298d61d051e8
Elena Ilyukhina <eil@ihtsdo.org>
2021-10-26 14:53:51 +0100

    Update WebSecurityConfig.java
    MAINT-734 Enable Global Method Security

--- identity-service/src/main/java/org/ihtsdo/otf/im/config/WebSecurityConfig.java
+++ identity-service/src/main/java/org/ihtsdo/otf/im/config/WebSecurityConfig.java
@@ -3,12 +3,14 @@ package org.ihtsdo.otf.im.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
