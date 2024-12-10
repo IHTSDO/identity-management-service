0957c22e0400344520825b1c0000fd4983657556
Darryn McGaw <dmcg@ihtsdo.org>
2024-12-10 12:49:20 +0000

    PIP-435 Fix compilation errors post upgrade to snomed-parent-bom

--- src/main/java/org/ihtsdo/otf/im/config/WebSecurityConfig.java
+++ src/main/java/org/ihtsdo/otf/im/config/WebSecurityConfig.java
@@ -1,29 +1,42 @@
 package org.ihtsdo.otf.im.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfig {
+	private static final String[] ANONYMOUS = {
+			"/api/reset_password", "/api/forgot_password"
+	};
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			.authorizeRequests()
-				.antMatchers("/api/reset_password", "/api/forgot_password").anonymous()
-				.antMatchers("/api/authenticate", "/api/account", "/api/account/logout", "/api/cache/**", "/api/user", "/api/group/**").permitAll()
-				.antMatchers("/api/**").authenticated();
-	}
+	private static final String[] PERMIT_ALL = {
+			"/v3/api-docs/**", "/swagger-ui/**",
+			"/api/authenticate", "/api/account", "/api/account/logout", "/api/cache/**",
+			"/api/user", "/api/group/**"
+	};
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-				.inMemoryAuthentication()
-				.withUser("user").password("password").roles("USER");
+	private static final String[] AUTHENTICATED = {
+			"/api/**"
+	};
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(authz -> authz
+						.requestMatchers(ANONYMOUS).anonymous()
+						.requestMatchers(PERMIT_ALL).permitAll()
+						.requestMatchers(AUTHENTICATED).authenticated()
+				)
+				.httpBasic(Customizer.withDefaults());
+
+		return http.build();
 	}
 }
