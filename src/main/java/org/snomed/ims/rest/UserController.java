da573eda625f400d9a7439a5a26d4392c4ced8a8
Quyen Ly <quocquyenit@gmail.com>
2025-03-07 14:40:28 +0700

    CRS-805 Add new endpoint to get roles for a user

--- src/main/java/org/snomed/ims/rest/UserController.java
+++ src/main/java/org/snomed/ims/rest/UserController.java
@@ -6,7 +6,12 @@ import org.snomed.ims.middle.CrowdRestClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Tag(name = "UserController")
@@ -28,4 +33,11 @@ public class UserController {
 			return new ResponseEntity<>(user, HttpStatus.OK);
 		}
 	}
+
+	@GetMapping(value = "/user/role", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<List<String>> getUserRoles(@RequestParam String username) {
+		return new ResponseEntity<>(crowdRestClient.getUserRoles(username), HttpStatus.OK);
+	}
+
 }
