9466ea64478b3ce0b2b36a66528cc5799c03e6ed
Chris Swires <csw@ihtsdo.org>
2025-08-13 14:38:17 +0100

    PIP-735 - added request debugging

--- src/main/java/org/snomed/ims/config/RestTemplateConfig.java
+++ src/main/java/org/snomed/ims/config/RestTemplateConfig.java
@@ -1,5 +1,7 @@
 package org.snomed.ims.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,8 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RestTemplateConfig.class);
+	
 	private final ApplicationProperties applicationProperties;
 
 	public RestTemplateConfig(ApplicationProperties applicationProperties) {
@@ -15,6 +19,7 @@ public class RestTemplateConfig {
 
 	@Bean(name = "crowd")
 	public RestTemplate crowdRestTemplate() {
+		LOGGER.info("Creating Crowd RestTemplate with rootUri: {}", applicationProperties.getCrowdApiUrl());
 		return new RestTemplateBuilder()
 				.rootUri(applicationProperties.getCrowdApiUrl())
 				.basicAuthentication(applicationProperties.getCrowdApiAppName(), applicationProperties.getCrowdApiAppPassword())
@@ -23,6 +28,7 @@ public class RestTemplateConfig {
 
 	@Bean(name = "keycloak")
 	public RestTemplate keyCloakRestTemplate() {
+		LOGGER.info("Creating Keycloak RestTemplate with rootUri: {}", applicationProperties.getKeycloakUrl());
 		return new RestTemplateBuilder()
 				.rootUri(applicationProperties.getKeycloakUrl())
 				.build();
